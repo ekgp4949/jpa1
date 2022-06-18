6cca54f53902c2cbfc9d22ca5a7ab587f97838c4
ekgp4949 <ekgp4949@gmail.com>
2022-06-18 13:33:52 +0900

    persistence test

---
+++ src/test/java/com/jpa/jpashop/MemberRepositoryTest.java
@@ -0,0 +1,29 @@
+package com.jpa.jpashop;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+class MemberRepositoryTest {
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    void testMember() {
+        Member member = new Member();
+        member.setUsername("member");
+
+        Long saveId = memberRepository.save(member);
+        Member saveMember = memberRepository.find(saveId);
+
+        Assertions.assertThat(saveMember).isSameAs(member);
+        Assertions.assertThat(saveMember.getId()).isEqualTo(saveId);
+        Assertions.assertThat(saveMember.getUsername()).isEqualTo("member");
+    }
+}
