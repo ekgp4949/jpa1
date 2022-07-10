a82dcc0d643da61628eae3c581e243e4bd27ba96
ekgp4949 <ekgp4949@gmail.com>
2022-07-10 22:33:42 +0900

    Order 비지니스 로직 추가

--- src/main/java/jpashop/repository/OrderRepository.java
+++ src/main/java/jpashop/repository/OrderRepository.java
@@ -3,8 +3,10 @@ package jpashop.repository;
 import jpashop.domain.Order;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 import java.util.List;
 
 @Repository
@@ -20,7 +22,36 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch) {
-//        return null;
+    public List<Order> findAllByString(OrderSearch orderSearch) {
+        String jpql ="select o from Order o join o.member m where 1=1";
+        if(orderSearch.getOrderStatus() != null) {
+            jpql += " and o.status = :status";
+        }
+
+        if(StringUtils.hasText(orderSearch.getMemberName())) {
+            jpql += " and m.name like :name";
+        }
+
+        TypedQuery<Order> query = em.createQuery(jpql, Order.class);
+        if(orderSearch.getOrderStatus() != null) {
+            query.setParameter("status", orderSearch.getOrderStatus());
+        }
+
+        if(StringUtils.hasText(orderSearch.getMemberName())) {
+            query.setParameter("name", orderSearch.getMemberName());
+        }
+        return query
+                .setMaxResults(1000)
+                .getResultList();
+    }
+
+    /**
+     * JPA Criteria 유지보수 어려움
+     */
+//    public List<Order> findAllByCriteria(OrderSearch orderSearch) {
+//        CriteriaBuilder cb = em.getCriteriaBuilder();
+//        CriteriaQuery<Order> query = cb.createQuery(Order.class);
+//
 //    }
+
 }
